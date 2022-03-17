eb12ac515f15b0753fb554bbd35549018cbe804b
Sang Dang <sangdth@gmail.com>
2022-03-17 10:52:26 +0200

    clean single job

--- screens/jobs/SingleJob.tsx
+++ screens/jobs/SingleJob.tsx
@@ -1,24 +1,12 @@
-import React, { useLayoutEffect, useMemo } from 'react';
+import React, { useLayoutEffect } from 'react';
 import styled from 'styled-components/native';
-import {
-  ConversationsAggregateDocument,
-  JobsByPkDocument,
-  InsertConversationsOneDocument,
-} from '@getants/graphqls';
-import { MainStackEnum, InboxStackEnum } from '../../utils/enums';
+import { JobsByPkDocument } from '@getants/graphqls';
 import type {
   JobsByPkQuery,
-  ConversationsAggregateQuery,
   SingleJobScreenRouteProp,
   SingleJobScreenNavigationProp,
 } from '../../utils/types';
-import {
-  useAuth,
-  useLazyQuery,
-  useQuery,
-  useMutation,
-  useTimeoutFn,
-} from '../../hooks';
+import { useQuery } from '../../hooks';
 import {
   Button,
   Card,
@@ -43,8 +31,6 @@ export type Props = {
 
 export const SingleJobScreen = (props: Props) => {
   const { navigation, route } = props;
-  const { user } = useAuth();
-  const userId = user?.id ?? '';
 
   const { jobId, jobTitle, companyName } = route.params;
 
@@ -64,83 +50,7 @@ export const SingleJobScreen = (props: Props) => {
     });
   }, [navigation, companyName, jobTitle]);
 
-  // One customer can have only ONE conversation with ONE company only.
-  // For different jobs, we notify with another signal over time.
-  const findConversationVars = {
-    limit: 1,
-    offset: 0,
-    where: {
-      _and: {
-        company_id: { _eq: job?.company?.id ?? '' },
-        users: { user_id: { _eq: userId } },
-      },
-    },
-  };
-
-  const [
-    findConversation,
-    {
-      data: aggregateData,
-      // error: aggregateError,
-      // loading: aggregateLoading,
-      // fetchMore: aggregateMore,
-      // refetch,
-    },
-  ] = useLazyQuery<ConversationsAggregateQuery>(
-    ConversationsAggregateDocument,
-    {
-      variables: findConversationVars,
-      fetchPolicy: 'cache-and-network',
-      notifyOnNetworkStatusChange: true,
-    },
-  );
-
-  const [, , reset] = useTimeoutFn(() => findConversation(), 100);
-
-  const conversation = useMemo(
-    () => aggregateData?.conversations_aggregate?.nodes[0],
-    [aggregateData],
-  );
-
-  const [addConversation, { data: newConversation }] = useMutation(
-    InsertConversationsOneDocument,
-  );
-
-  const navigateToConversation = (id: string) => {
-    navigation.navigate(MainStackEnum.InboxStack, {
-      screen: InboxStackEnum.ConversationScreen,
-      params: {
-        conversationId: id,
-        userId,
-        jobId,
-      },
-    });
-  };
-
-  const handleApply = async () => {
-    if (conversation) {
-      navigateToConversation(conversation?.id);
-    } else {
-      await addConversation({
-        variables: {
-          object: {
-            name: job?.company?.name,
-            description: job?.title,
-            status: 'STARTED',
-            company_id: job?.company?.id,
-            users: {
-              data: { user_id: userId },
-            },
-          },
-        },
-      });
-
-      if (newConversation) {
-        navigateToConversation(newConversation?.id);
-      }
-    }
-    reset();
-  };
+  const handleApply = async () => {};
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
